fc4ad0c0670a11ba228275f416d0b569eed3e026
Murad Najafli <muradnecefli05@gmail.com>
2023-05-26 12:17:30 +0400

    CycleSwapTask

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,31 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+        int lastElement = array[array.length-1];
+        for (int i = array.length-1; i > 0; i--) {
+            array[i] = array[i-1];
+        }
+        array[0] = lastElement;
 
     }
 
     static void cycleSwap(int[] array, int shift) {
-
+        if (array == null || array.length == 0 || shift == 0) {
+            return;
+        }
+        int length = array.length;
+        shift = shift % length;
+        for (int i = 0; i < shift; i++) {
+            int lastElement = array[length-1];
+            for (int j = length-1; j > 0; j--) {
+                array[j] = array[j-1];
+            }
+            array[0] = lastElement;
+            
+        }
     }
 }
+
